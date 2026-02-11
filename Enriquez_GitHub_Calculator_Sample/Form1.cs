b93207c13a6a8198f2a696feceedc394bc539baf
NeonWandererCoder <enriquez.287395@balagtas.sti.edu.ph>
2026-02-11 19:03:45 +0800

    FEAT: ADD PROJECT

--- Enriquez_GitHub_Calculator_Sample/Form1.cs
+++ Enriquez_GitHub_Calculator_Sample/Form1.cs
@@ -16,5 +16,10 @@ namespace Enriquez_GitHub_Calculator_Sample
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
